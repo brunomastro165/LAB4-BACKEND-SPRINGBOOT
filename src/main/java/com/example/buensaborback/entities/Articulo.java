7a26f1fe276149695d58a56b22351fa47e5bce76
brunomastro165 <brunomastro165@gmail.com>
2024-04-25 23:08:35 -0300

    pruebas finales

--- src/main/java/com/example/buensaborback/entities/Articulo.java
+++ src/main/java/com/example/buensaborback/entities/Articulo.java
@@ -14,16 +14,16 @@ import java.util.Set;
 @ToString
 @Setter
 @SuperBuilder
+
+//Genera UNA TABLA para cada CLASE que HEREDA de esta
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Articulo extends Base {
 
     protected String denominacion;
     protected Double precioVenta;
 
     @OneToMany
-    //SE AGREGA EL JOIN COLUMN PARA QUE JPA NO CREE LA TABLA INTERMEDIA EN UNA RELACION ONE TO MANY
-    //DE ESTA MANERA PONE EL FOREIGN KEY 'cliente_id' EN LA TABLA DE LOS MANY
     @JoinColumn(name = "articulo_id")
-    //SE AGREGA EL BUILDER.DEFAULT PARA QUE BUILDER NO SOBREESCRIBA LA INICIALIZACION DE LA LISTA
     @Builder.Default
     private Set<Imagen> imagenes = new HashSet<>();
 
