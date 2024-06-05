66f3d0e522178307739f20cea7b25907e90efc97
SR0jo <santiagomza12@gmail.com>
2024-06-05 17:19:29 -0300

    la cosa que mas me ha dolido

--- src/main/java/com/example/buensaborback/presentation/rest/ArticuloManufacturadoController.java
+++ src/main/java/com/example/buensaborback/presentation/rest/ArticuloManufacturadoController.java
@@ -8,9 +8,19 @@ import com.example.buensaborback.domain.dto.ArticuloManufacturado.ArticuloManufa
 import com.example.buensaborback.domain.dto.ArticuloManufacturado.ArticuloManufacturadoDto;
 import com.example.buensaborback.domain.dto.ArticuloManufacturado.ArticuloManufacturadoEditDto;
 import com.example.buensaborback.domain.dto.ImagenArticuloDto.ImagenArticuloDto;
+import com.example.buensaborback.domain.dto.ImagenPromocion.ImagenPromocionDto;
+import com.example.buensaborback.domain.dto.Promocion.PromocionCreateDto;
+import com.example.buensaborback.domain.dto.Promocion.PromocionDto;
+import com.example.buensaborback.domain.dto.PromocionDetalle.PromocionDetalleCreateDto;
 import com.example.buensaborback.domain.entities.ArticuloManufacturado;
+import com.example.buensaborback.domain.entities.ImagenArticulo;
+import com.example.buensaborback.domain.entities.Promocion;
+import com.example.buensaborback.domain.entities.PromocionDetalle;
 import com.example.buensaborback.presentation.base.BaseControllerImpl;
+import com.example.buensaborback.repositories.ArticuloManufacturadoRepository;
+import com.example.buensaborback.repositories.ArticuloRepository;
 import com.example.buensaborback.repositories.CategoriaRepository;
+import com.example.buensaborback.repositories.ImagenArticuloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +29,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -28,9 +39,12 @@ import java.util.stream.Collectors;
 public class ArticuloManufacturadoController extends BaseControllerImpl<ArticuloManufacturado, ArticuloManufacturadoDto, ArticuloManufacturadoCreateDto, ArticuloManufacturadoEditDto, Long, ArticuloManufacturadoFacadeImpl> {
     @Autowired
     private ImagenArticuloService imageService;
-
+    @Autowired
+    private ImagenArticuloRepository imagenArticuloRepository;
     @Autowired
     private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ArticuloManufacturadoRepository articuloRepository;
 
     public ArticuloManufacturadoController(ArticuloManufacturadoFacadeImpl facade) {
         super(facade);
@@ -45,6 +59,15 @@ public class ArticuloManufacturadoController extends BaseControllerImpl<Articulo
                 .collect(Collectors.toList());
         return ResponseEntity.ok(filteredArticulos);
     }
+    @PutMapping(value = "save/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<ArticuloManufacturadoDto> edit(@RequestPart("entity") ArticuloManufacturadoEditDto entity,
+                                                         @RequestPart("files") MultipartFile[] files, @PathVariable Long id) {
+        facade.update(entity,id);
+        imageService.uploadImagesA(files, id);
+
+        return ResponseEntity.ok(facade.getById(id));
+    }
+
 
     @PostMapping(value = "/save", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ArticuloManufacturadoDto> create(@RequestPart("entity") ArticuloManufacturadoCreateDto entity, @RequestPart("files") MultipartFile[] files) {
