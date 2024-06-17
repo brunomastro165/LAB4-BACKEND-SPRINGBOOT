f3285a5a84d8ecc5a560dbc657f9bbf87911d5bd
SR0jo <santiagomza12@gmail.com>
2024-06-17 20:37:03 -0300

    prueba del render v3

--- src/main/java/com/example/buensaborback/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/example/buensaborback/configuration/security/SecurityConfiguration.java
@@ -1,5 +1,5 @@
 package com.example.buensaborback.configuration.security;
-/*
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
